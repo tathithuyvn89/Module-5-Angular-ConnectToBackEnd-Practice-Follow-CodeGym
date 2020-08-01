15206fe5cb7b6c0589148a694c1aa75c8c2608c7
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2020-08-01 20:23:59 +0700

    Practice connect to Backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {TokenInterceptor} from './token.interceptor';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    BlogEditComponent,
+    BlogDetailComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/blog-detail/blog-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {IPost} from '../IPost';
+import {ActivatedRoute} from '@angular/router';
+import {PostService} from '../post.service';
+
+@Component({
+  selector: 'app-blog-detail',
+  templateUrl: './blog-detail.component.html',
+  styleUrls: ['./blog-detail.component.css']
+})
+export class BlogDetailComponent implements OnInit {
+
+  post: IPost;
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.postService.getPostById(id).subscribe(
+      next => (this.post = next),
+      error => {
+        console.log(error);
+        this.post = null;
+      }
+    );
+  }
+
+}
